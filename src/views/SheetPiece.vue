c6a483f026807478c4118bc33fcb5445276f82de
juanjo810 <juanjo_810@usal.es>
2023-09-12 14:15:29 +0100

    piece sheet

--- src/views/SheetPiece.vue
+++ src/views/SheetPiece.vue
@@ -1,115 +1,46 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <v-card-text>
-          <v-row>
-            <v-col cols="12">
-              <SearchBar
-                :objects="this.getNamePieces"
-                :getInfo="this.loadPieceInfo"
-              ></SearchBar>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12">
-              <v-card-title class="text-h5" v-if="this.content">{{
-                this.pieceForm.title
-              }}</v-card-title>
-              <v-card-text v-else
-                >This piece doesn't contain any MEI file. You can edit the piece
-                and upload it.</v-card-text
-              >
-              <VerovioCanvas
-                v-if="this.content"
-                :toolkit="toolkit"
-                :data="this.content"
-                view-mode="vertical"
-              />
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-row>
-              <v-col cols="12">
-                <input type="file" accept=".xml" @change="handleFileChange2" />
-              </v-col>
-            </v-row>
-            <v-col cols="12">
-              <v-card-text v-if="!this.midiFilePath"
-                >This piece doesn't contain any MIDI file. You can edit the
-                piece and upload it.</v-card-text
-              >
-              <v-btn v-else @click="this.playMidi = !this.playMidi">
-                {{ this.playMidi ? "Stop" : "Play" }}
-              </v-btn>
-              <MidiAudioPlayer
-                :midi="this.midiFilePath"
-                :playMidi="this.playMidi"
-                @finishedPlaying="this.playMidi = false"
-              />
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <div ref="osmdContainer"></div>
+    <input type="file" @change="onFileChange" />
+  </div>
 </template>
 
-<script setup>
-const toolkit = ref(null);
-createVerovioModule().then((VerovioModule) => {
-  toolkit.value = createAsyncVerovioToolkit(new VerovioToolkit(VerovioModule));
-});
-</script>
-
 <script>
-import { mapActions, mapGetters, mapState } from "vuex";
-import "vue-verovio-canvas/style.css";
-import { VerovioCanvas, createAsyncVerovioToolkit } from "vue-verovio-canvas";
-import { VerovioToolkit } from "verovio/esm";
-import createVerovioModule from "verovio/wasm-hum";
-import { ref } from "vue";
-import MidiAudioPlayer from "@/components/MidiAudioPlayer.vue";
-import SearchBar from "@/components/SearchBar.vue";
+import * as opensheetmusicdisplay from 'opensheetmusicdisplay'; // Importa la biblioteca
 
 export default {
-  components: {
-    VerovioCanvas,
-    MidiAudioPlayer,
-    SearchBar,
-  },
   data() {
     return {
-      content: null,
-      midiFilePath: null,
-      playMidi: false,
+      osmd: null,
     };
   },
-  computed: {
-    ...mapGetters(["getMei", "getMidi", "getNamePieces"]),
-    ...mapState(["pieceForm"]),
-  },
   methods: {
-    ...mapActions(["getPieceInfo"]),
-    handleFileChange2(event) {
-      debugger;
-      const file = event.target.files[0];
-      const reader = new FileReader();
+    onFileChange(e) {
+      var reader = new FileReader();
       reader.onload = () => {
-        this.midiFilePath = reader.result;
+        this.osmd.load(reader.result).then(
+          () => {
+            this.osmd.render();
+          },
+          function (e) {
+            console.log('error loading ' + e);
+          }
+        );
       };
-      reader.readAsBinaryString(file);
-    },
-    async loadMedia() {
-      this.content = await this.getMei;
-      debugger;
-      this.midiFilePath = this.getMidi;
-    },
-    async loadPieceInfo(selectedPiece) {
-      const parts = selectedPiece.split("-");
-      await this.getPieceInfo({ piece: parts });
-      this.loadMedia();
+      reader.readAsText(e.target.files[0]);
     },
   },
+  mounted() {
+    this.osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(this.$refs.osmdContainer);
+    this.osmd.setOptions({
+      backend: 'svg',
+      drawTitle: true,
+      // drawingParameters: 'compacttight' // Opciones adicionales si es necesario
+    });
+  }
 };
 </script>
+
+<style scoped>
+/* Estilos específicos para este componente si es necesario */
+</style>
